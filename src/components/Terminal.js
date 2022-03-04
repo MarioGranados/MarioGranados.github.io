8450cefe0aac808c57c9394f80d84ae271150155
Mario Granados <mario.alberto.granados1@gmail.com>
2022-03-04 10:28:21 -0600

    v2

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -11,13 +11,13 @@ const Terminal = () => {
                 <div className="container">
                     <form className='justify-content-center d-flex align-items-center'>
                         <div className="form-group row">
-                            <label htmlFor="name" className="text-success">Enter Your Name: </label>
+                            <label htmlFor="name" className="text-success">guest@guestUser~ % Enter Your Name: </label>
                             <div className="px-5">
                                 <input type="text" className="form-control bg-dark border-success text-success" value={name} onChange={e => setName(e.target.value)}/>
                             </div>
                         </div>
                     </form>
-                    <p className="text-success">{name.length > 3 ? `guest@user-MBP % Welcome ${name} Nice To Meet You` :  "" }</p>
+                    <p className="text-success">{name.length > 3 ? `guest@${name}User~ % Welcome ${name} Nice To Meet You` :  "" }</p>
                     {name.length > 3 ?
                         <div className="form-group row">
                         <label htmlFor="input" className="text-success">$: Where would you like to go? </label>
