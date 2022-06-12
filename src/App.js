bf0789558a3395e02d9174bd5801dbc8b1211f2b
Mario Granados <mario.alberto.granados1@gmail.com>
2022-06-11 22:28:40 -0500

    scratch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
+import Navbar from './components/Navbar'
 import React from "react";
-import Aos from "aos";
-import 'aos/dist/aos.css';
-import Hero from "./components/Hero";
 import './App.css';
-import Footer from "./components/Footer";
-import GridScroll from "./components/Portfolio/GridScroll";
-import Terminal from "./components/Terminal";
-import {experience, about} from "./components/Data";
-import Loading from "./components/Loading";
-import WelcomePage from "./components/WelcomePage";
+import Banner from "./components/Banner";
 
 function App() {
-    Aos.init();
     return (
         <>
-            <div className='main_bg'>
-                <Terminal/>
-                <WelcomePage/>
-                <Hero {...experience} />
-                <Hero {...about} />
-                <GridScroll/>
-                <Footer/>
-            </div>
+            <Navbar/>
+            <Banner/>
         </>
     );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,47 +1,10 @@
-
-
-const Hero = ({inverse, img, alt, title, content}) => {
-
-    let imgStyling = {
-        width: '100%',
-        height: '100%',
-        borderRadius: '5px',
-    }
-
-    let cols = () => {
-        if (inverse) {
-            return (<>
-                <div className="col text-success">
-                    {content}
-                </div>
-                <div className="col text-success">
-                    <img style={imgStyling} src={img} alt={alt}/>
-                </div>
-            </>);
-        } else {
-            return (
-                <>
-                <div className="col text-success">
-                    <img style={imgStyling} src={img} alt={alt}/>
-                </div>
-                <div className="col text-success">
-                    {content}
-                </div>
-            </>
-            );
-        }
-    }
-
-    return (
+const Hero = () => {
+    return(
         <>
-            <div className="container green_border my-5 py-2">
-                <div className="text-center display-4">{title}</div>
-                <div className="row justify-content-around p-5">
-                    {cols(inverse)}
-                </div>
+            <div>
+                Hello World!
             </div>
         </>
     );
-
 }
 export default Hero;
