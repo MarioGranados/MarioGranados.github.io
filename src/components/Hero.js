c8ded58ba11f15b0a1d3db76844ef0ca9bfbce28
Mario Granados <mario.alberto.granados1@gmail.com>
2022-06-12 01:03:39 -0500

    hero added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,12 @@ const Hero = () => {
         <>
             <div className="hero">
                 <div className="container">
-                    <div className="col_left">
+                    <div className="col_left box_effect box col">
                         <h4>title</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis consectetur consequuntur
                             doloribus eaque, iusto placeat!</p>
                     </div>
-                    <div className="col_right">
+                    <div className="col_right box_gloss box col">
                         <h3>title again</h3>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex iure minus optio soluta velit
