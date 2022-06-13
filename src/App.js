28ba7ba3319dfae6568ac5f18c48edacafcdba5d
Mario Granados <mario.alberto.granados1@gmail.com>
2022-06-12 19:46:04 -0500

    sidemenu

--- src/App.js
+++ src/App.js
@@ -8,21 +8,38 @@ import {recycode, weatherApp, movieApp, experience} from "./components/Data";
 import OtherProjects from "./components/OtherProjects";
 import About from "./components/About";
 import {Headers} from "./components/Headers";
+import {FaGithub, FaLinkedin} from "react-icons/fa";
+import {SiLeetcode} from "react-icons/si";
+import {MdMail} from "react-icons/md";
 
 function App() {
     return (
         <>
-            <Navbar/>
-            <Banner/>
-            <Headers>Experience</Headers>
-            <About {...experience}/>
-            <Headers>Some Of My Projects</Headers>
-            <Hero {...recycode}/>
-            <Hero {...weatherApp}/>
-            <Hero {...movieApp}/>
-            <Headers>Other Cool Projects</Headers>
-            <OtherProjects/>
-            <Footer/>
+            <div className='sidenav d-flex '>
+                <a href="/" className='sidenav_links'>
+                    <FaGithub/>
+                </a>
+                <a href="/" className='sidenav_links'>
+                    <FaLinkedin/>
+                </a>
+                <a href="/" className='sidenav_links'>
+                    <MdMail/>
+                </a>
+                <a href="/" className='sidenav_links'>
+                    <SiLeetcode/>
+                </a>
+            </div>
+                <Navbar/>
+                <Banner/>
+                <Headers>Experience</Headers>
+                <About {...experience}/>
+                <Headers>Some Of My Projects</Headers>
+                <Hero {...recycode}/>
+                <Hero {...weatherApp}/>
+                <Hero {...movieApp}/>
+                <Headers>Other Cool Projects</Headers>
+                <OtherProjects/>
+                <Footer/>
         </>
     );
 }
