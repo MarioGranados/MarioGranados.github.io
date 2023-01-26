960272066429a3d3931e178dee1ab2242f80ddda
Mario Granados <mario.alberto.granados1@gmail.com>
2023-01-25 21:03:48 -0600

    v4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import Navbar from './components/Navbar'
-import React from "react";
-import './App.css';
-import Banner from "./components/Banner";
+
 
 function App() {
     return (
         <>
-            <Navbar/>
-            <Banner/>
+
         </>
     );
 }
