2ce82a40e245a0eb9a9dcbf6a9181b65ade2e1ff
Mario Granados <mario.alberto.granados1@gmail.com>
2023-01-26 00:02:12 -0600

    adding port

--- src/Components/MoreProjects.jsx
+++ src/Components/MoreProjects.jsx
@@ -1,12 +1,14 @@
+import COFFEE from '../Images/coffee.png'
+import MOVIE from '../Images/movies.png'
 const MoreProjects = () => {
   return (
     <>
       <div className="container px-4 py-5" id="custom-cards">
         <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
           <div className="col">
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{backgroundImage: `url(${MOVIE})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
               <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Title</h3>
+                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Coffee Project</h3>
                 <ul className="d-flex list-unstyled mt-auto">
                   <li className="me-auto">
                     <img
@@ -35,7 +37,7 @@ const MoreProjects = () => {
           </div>
 
           <div className="col">
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{backgroundImage: `url(${COFFEE})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
               <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                 <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Title</h3>
                 <ul className="d-flex list-unstyled mt-auto">
