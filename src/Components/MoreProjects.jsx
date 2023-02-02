53abb0916ba0c47929fa724b2606d41d2ede9eb1
Mario Granados <mario.alberto.granados1@gmail.com>
2023-02-02 08:54:36 -0600

    links

--- src/Components/MoreProjects.jsx
+++ src/Components/MoreProjects.jsx
@@ -1,19 +1,28 @@
-import COFFEE from '../Images/coffee.png'
-import MOVIE from '../Images/movies.png'
-import WEATHER from '../Images/weather.png'
-import LOGO from '../Images/m.png'
+import COFFEE from "../Images/coffee.png";
+import MOVIE from "../Images/movies.png";
+import WEATHER from "../Images/weather.png";
+import LOGO from "../Images/m.png";
 const MoreProjects = () => {
   return (
-    <div className='container'>
-
-        <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-          <div className="col" data-aos="fade-up">
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${MOVIE})`}}>
+    <div className="container">
+      <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+        <div className="col" data-aos="fade-up">
+          <a href="https://mariogranados.github.io/MovieProject/">
+            <div
+              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style={{
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${MOVIE})`,
+              }}
+            >
               <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Movie Project</h3>
+                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                  Movie Project
+                </h3>
                 <ul className="d-flex list-unstyled mt-auto">
                   <li className="me-auto">
-                  <img
+                    <img
                       src={LOGO}
                       alt="MG"
                       width="32"
@@ -36,71 +45,93 @@ const MoreProjects = () => {
                 </ul>
               </div>
             </div>
-          </div>
-
-          <div className="col" data-aos='fade-down'>
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${COFFEE})`}}>
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Coffee Project</h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                  <img
-                      src={LOGO}
-                      alt="MG"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill" />
-                    </svg>
-                    <small>HTML, CSS, JS</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#calendar3" />
-                    </svg>
-                    <small></small>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="col" data-aos='fade-up'>
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style={{ backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${WEATHER})`}}>
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Weather Map Project</h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                  <img
-                      src={LOGO}
-                      alt="MG"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill" />
-                    </svg>
-                    <small>HTML, CSS, JS</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#calendar3" />
-                    </svg>
-                    <small></small>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          </a>
         </div>
-  
+
+        <div className="col" data-aos="fade-down">
+          <a href="https://mariogranados.github.io/Coffee-ProjectV2/">
+            <div
+              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style={{
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${COFFEE})`,
+              }}
+            >
+              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                  Coffee Project
+                </h3>
+                <ul className="d-flex list-unstyled mt-auto">
+                  <li className="me-auto">
+                    <img
+                      src={LOGO}
+                      alt="MG"
+                      width="32"
+                      height="32"
+                      className="rounded-circle border border-white"
+                    />
+                  </li>
+                  <li className="d-flex align-items-center me-3">
+                    <svg className="bi me-2" width="1em" height="1em">
+                      <use xlinkHref="#geo-fill" />
+                    </svg>
+                    <small>HTML, CSS, JS</small>
+                  </li>
+                  <li className="d-flex align-items-center">
+                    <svg className="bi me-2" width="1em" height="1em">
+                      <use xlinkHref="#calendar3" />
+                    </svg>
+                    <small></small>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div className="col" data-aos="fade-up">
+          <a href="https://mariogranados.github.io/weather-map-app/">
+            <div
+              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+              style={{
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(${WEATHER})`,
+              }}
+            >
+              <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                  Weather Map Project
+                </h3>
+                <ul className="d-flex list-unstyled mt-auto">
+                  <li className="me-auto">
+                    <img
+                      src={LOGO}
+                      alt="MG"
+                      width="32"
+                      height="32"
+                      className="rounded-circle border border-white"
+                    />
+                  </li>
+                  <li className="d-flex align-items-center me-3">
+                    <svg className="bi me-2" width="1em" height="1em">
+                      <use xlinkHref="#geo-fill" />
+                    </svg>
+                    <small>HTML, CSS, JS</small>
+                  </li>
+                  <li className="d-flex align-items-center">
+                    <svg className="bi me-2" width="1em" height="1em">
+                      <use xlinkHref="#calendar3" />
+                    </svg>
+                    <small></small>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import LOGO from '../Images/m.png'
+import LOGO from "../Images/m.png";
 import MoreProjects from "../Components/MoreProjects";
 const Portfolio = () => {
   const [showMore, setShowMore] = useState(false);
@@ -13,108 +13,121 @@ const Portfolio = () => {
         <h2 className="pb-2 border-bottom">My Works</h2>
 
         <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-          <div className="col" data-aos='fade-up'>
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">PokeLister</h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                    <img
-                      src={LOGO}
-                      alt="MG"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill" />
-                    </svg>
-                    <small></small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#calendar3" />
-                    </svg>
-                    <small></small>
-                  </li>
-                </ul>
+          <div className="col" data-aos="fade-up">
+            <a href="">
+              <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+                <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                  <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                    PokeLister
+                  </h3>
+                  <ul className="d-flex list-unstyled mt-auto">
+                    <li className="me-auto">
+                      <img
+                        src={LOGO}
+                        alt="MG"
+                        width="32"
+                        height="32"
+                        className="rounded-circle border border-white"
+                      />
+                    </li>
+                    <li className="d-flex align-items-center me-3">
+                      <svg className="bi me-2" width="1em" height="1em">
+                        <use xlinkHref="#geo-fill" />
+                      </svg>
+                      <small></small>
+                    </li>
+                    <li className="d-flex align-items-center">
+                      <svg className="bi me-2" width="1em" height="1em">
+                        <use xlinkHref="#calendar3" />
+                      </svg>
+                      <small></small>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
+            </a>
           </div>
 
-          <div className="col" data-aos='fade-down'>
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">RecyCode</h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                  <img
-                      src={LOGO}
-                      alt="MG"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill" />
-                    </svg>
-                    <small>Pakistan</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#calendar3" />
-                    </svg>
-                    <small></small>
-                  </li>
-                </ul>
+          <div className="col" data-aos="fade-down">
+            <a href="">
+              <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+                <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                  <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                    RecyCode
+                  </h3>
+                  <ul className="d-flex list-unstyled mt-auto">
+                    <li className="me-auto">
+                      <img
+                        src={LOGO}
+                        alt="MG"
+                        width="32"
+                        height="32"
+                        className="rounded-circle border border-white"
+                      />
+                    </li>
+                    <li className="d-flex align-items-center me-3">
+                      <svg className="bi me-2" width="1em" height="1em">
+                        <use xlinkHref="#geo-fill" />
+                      </svg>
+                      <small>Pakistan</small>
+                    </li>
+                    <li className="d-flex align-items-center">
+                      <svg className="bi me-2" width="1em" height="1em">
+                        <use xlinkHref="#calendar3" />
+                      </svg>
+                      <small></small>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
+            </a>
           </div>
 
-          <div className="col" data-aos='fade-up'>
-            <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">PokeApp</h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                  <img
-                      src={LOGO}
-                      alt="MG"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#geo-fill" />
-                    </svg>
-                    <small>California</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <svg className="bi me-2" width="1em" height="1em">
-                      <use xlinkHref="#calendar3" />
-                    </svg>
-                    <small>5d</small>
-                  </li>
-                </ul>
+          <div className="col" data-aos="fade-up">
+            <a href="">
+              <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
+                <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+                  <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
+                    PokeApp
+                  </h3>
+                  <ul className="d-flex list-unstyled mt-auto">
+                    <li className="me-auto">
+                      <img
+                        src={LOGO}
+                        alt="MG"
+                        width="32"
+                        height="32"
+                        className="rounded-circle border border-white"
+                      />
+                    </li>
+                    <li className="d-flex align-items-center me-3">
+                      <svg className="bi me-2" width="1em" height="1em">
+                        <use xlinkHref="#geo-fill" />
+                      </svg>
+                      <small>California</small>
+                    </li>
+                    <li className="d-flex align-items-center">
+                      <svg className="bi me-2" width="1em" height="1em">
+                        <use xlinkHref="#calendar3" />
+                      </svg>
+                      <small>5d</small>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
+            </a>
           </div>
         </div>
         {!showMore ? (
           <button className="btn-outline-dark btn" onClick={handleClick}>
             Show More
           </button>
-        ) : (<>
-            <MoreProjects/>
-          <button className="btn-outline-dark btn" onClick={handleClick}>
-            Show Less
-          </button>
+        ) : (
+          <>
+            <MoreProjects />
+            <button className="btn-outline-dark btn" onClick={handleClick}>
+              Show Less
+            </button>
           </>
         )}
       </div>
